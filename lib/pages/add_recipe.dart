3548fe578bf236e53574fdc004908c1b0f96285b
FilipMajewsk1 <filip.majewski.022@gmail.com>
2023-04-30 14:34:58 +0200

    Recipe book: add_recipe steps and ingredients scrollbars and textfields with buttons

--- lib/pages/add_recipe.dart
+++ lib/pages/add_recipe.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_book_ui/pages/widgets/recipe_ingredients_steps_card.dart';
 
 class AddRecipe extends StatefulWidget {
   const AddRecipe({Key? key}) : super(key: key);
@@ -8,6 +9,13 @@ class AddRecipe extends StatefulWidget {
 }
 
 class _AddRecipeState extends State<AddRecipe> {
+
+  TextEditingController ingController = TextEditingController();
+  TextEditingController stepController = TextEditingController();
+  late String name;
+  List<String> ing = [];
+  List<String> stp = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +29,120 @@ class _AddRecipeState extends State<AddRecipe> {
         ),
         backgroundColor: Colors.purple[700],
         centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 20, 140, 5),
+                child: Text(
+                  "Ingredients",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 150,
+                width: 250,
+                child: Scrollbar(
+                  child: ListView(
+                    children: ing.map((i) => RecipeIngredientsStepsCard(ingredientOrStep: i)).toList(),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 300,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: ingController,
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: "Enter ingredient",
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10,20,0,0),
+                      child: ElevatedButton.icon(
+                        onPressed: (){
+                          setState(() {
+                            ing.add(ingController.text);
+                          });
+                          ingController.clear();
+                        },
+                        icon: Icon(Icons.add),
+                        label: Text("Add") ,
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(Colors.purple[700]),
+                          minimumSize: MaterialStateProperty.all(Size(100, 35)),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 20, 185, 5),
+                child: Text(
+                  "Steps",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 150,
+                width: 250,
+                child: Scrollbar(
+                  child: ListView(
+                    children: stp.map((s) => RecipeIngredientsStepsCard(ingredientOrStep: s)).toList(),
+                  ),
+                ),
+              ),
+
+              SizedBox(
+                width: 300,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: stepController,
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: "Enter step",
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10,20,0,0),
+                      child: ElevatedButton.icon(
+                        onPressed: (){
+                          setState(() {
+                            stp.add(stepController.text);
+                          });
+                          stepController.clear();
+                        },
+                        icon: Icon(Icons.add),
+                        label: Text("Add") ,
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(Colors.purple[700]),
+                          minimumSize: MaterialStateProperty.all(Size(100, 35)),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
